e5cf0322bfe5a5a5b8ff33a664397e3c383b54e0
SashaTolkodubova <konstantin@Konstantins-Mac-mini.local>
2024-03-25 23:42:58 +0300

    add code on step3

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class AppTest {
+    @Test
+    public void firstTest() {
+        Assertions.assertTrue(true);
+    }
+}
